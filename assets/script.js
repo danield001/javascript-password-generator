31ff8b6ca01aab9ae7e0d3ca8d695252da293f21
danield001 <30894506+danield001@users.noreply.github.com>
2023-09-27 23:41:18 +0930

    finalised JavaCode

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var passwordText = document.querySelector("#password");
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    
 
-  passwordText.value = password;
+  var length = 12;
+  var alpha = 'abcdefghijklmnopqrstuvwxyz';
+  var alphaUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbers = '1234567890';
+  var symbols = '!@#$%^&*()';
 
+let password = '';
+
+
+while (password.length < length){
+    const characterSet = alpha + alphaUpper + numbers + symbols;
+    const character = characterSet[Math.floor(Math.random() * characterSet.length)];
+
+        password += character;
+        
+    }
+
+     passwordText.value = password;
+    
 }
+ 
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+    
+    
