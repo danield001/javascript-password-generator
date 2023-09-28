98055e6694977f129d45b748460dc82a82894787
danield001 <30894506+danield001@users.noreply.github.com>
2023-09-28 16:44:01 +0930

    addition of password constraints to Java Code

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,70 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
+
+function checkLength() {
+
+  var pLength = document.querySelector("#length").value;
+  var length = parseInt(pLength);
+  var password = "";
+
+  if (length < 8 || length > 128) {
+    passwordText.value = "PLEASE ENTER NUMBER GREATER THAN 8 AND LESS THAN 128";
+    return false;
+  }
+  return true;
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
-    
+  if (!checkLength()) {
+    return
+  }
 
-  var length = 12;
+  var length = parseInt(document.querySelector("#length").value);
+  var numbersCheck = document.querySelector("#numbers").checked;
+  var upperCheck = document.querySelector("#upper-case").checked;
+  var symbolCheck = document.querySelector("#symbols").checked;
   var alpha = 'abcdefghijklmnopqrstuvwxyz';
   var alphaUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var numbers = '1234567890';
   var symbols = '!@#$%^&*()';
 
-let password = '';
+  let password = '';
 
+  if (numbersCheck) {
 
-while (password.length < length){
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+  }
+
+  if (upperCheck) {
+
+    password += alphaUpper[Math.floor(Math.random() * alphaUpper.length)];
+  }
+
+  if (symbolCheck) {
+
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+  }
+
+  while (password.length < length) {
     const characterSet = alpha + alphaUpper + numbers + symbols;
     const character = characterSet[Math.floor(Math.random() * characterSet.length)];
 
-        password += character;
-        
-    }
+    password += character;
 
-     passwordText.value = password;
-    
+  }
+
+  passwordText.value = password;
+
 }
- 
+
 
 
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-    
-    
+
